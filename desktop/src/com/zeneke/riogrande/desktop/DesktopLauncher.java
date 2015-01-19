fec8b9fc97d87e5b28fd365a924ce0ee80558f0f
unknown <zeneke@gmail.com>
2015-01-19 23:36:12 +0100

    desktop con el META-INF

---
+++ desktop/src/com/zeneke/riogrande/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.zeneke.riogrande.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.zeneke.riogrande.RiograndeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RiograndeGame(), config);
+	}
+}
